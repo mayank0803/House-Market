0c206d004c7ebec05da47dd2ad1cbeacc06346ba
mayank0803 <mayanksharma.0803@gmail.com>
2022-12-19 18:35:48 +0530

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,31 @@
+// import { initializeApp } from 'firebase/app'
+// import { getFirestore } from 'firebase/firestore'
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDA8LVcBB6ZuFMGtZZLEh_veJ44WGrNRdE',
+//   authDomain: 'house-marketplace-app-fb1d0.firebaseapp.com',
+//   projectId: 'house-marketplace-app-fb1d0',
+//   storageBucket: 'house-marketplace-app-fb1d0.appspot.com',
+//   messagingSenderId: '832068369979',
+//   appId: '1:832068369979:web:dce177da9bfc60a4b4e61e',
+// }
+
+// // Initialize Firebase
+// initializeApp(firebaseConfig)
+// export const db = getFirestore()
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ7XegUyDN7ShDTkwH9P11NZxckS56dbg",
+  authDomain: "house-marketplace-878eb.firebaseapp.com",
+  projectId: "house-marketplace-878eb",
+  storageBucket: "house-marketplace-878eb.appspot.com",
+  messagingSenderId: "948671267772",
+  appId: "1:948671267772:web:3c0abe81818eb765a9a1b9"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
